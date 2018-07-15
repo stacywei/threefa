5e86fbf302263bca109525dbf9e8116f11a9e1c1
aitong yu <aiy@aitongs-MBP.fios-router.home>
2018-07-15 12:44:06 -0400

    update to the company page

---
+++ Threefa/src/component/SimilarData.js
@@ -0,0 +1,23 @@
+import p1 from '../p1.jpg';
+import Ramen from '../Ramen.jpg';
+import p2 from '../p2.jpg';
+ 
+  const similarData = [
+    {
+      img: p1,
+      title: 'Restaurant name',
+      about: 'onesentence',
+    },
+    {
+      img: Ramen,
+      title: 'Name',
+      about: 'onesentence',
+    },
+    {
+      img: p2,
+      title: 'Name',
+      about: 'onesentence',
+    },
+  ];
+
+  export default similarData;
