ae8f57c39e6e8bc70b20bda5665e387d0766aae0
unknown <sc2552@cornell.edu>
2018-08-08 20:41:07 -0400

    changed color

--- Threefa/src/component/Home.js
+++ Threefa/src/component/Home.js
@@ -5,7 +5,7 @@ import '../styles/Home.css';
 //import BigCard from './bigcard'
 import MainCarousel from './MainCarousel';
 import Navbar from './Navbar';
-import Tabfilter from './list'
+import Tabfilter from './List'
 import Fave from './Fave.js'
 
  
