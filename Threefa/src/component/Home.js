32544f3413600ef75db126d52fd9152b85a09a59
Stacy <sw653@cornell.edu>
2019-01-05 18:25:32 -0600

    Updated vulnerabilities and fixed minor errors

--- Threefa/src/component/Home.js
+++ Threefa/src/component/Home.js
@@ -5,20 +5,20 @@ import '../styles/Home.css';
 //import BigCard from './bigcard'
 import MainCarousel from './MainCarousel';
 import Navbar from './Navbar';
-import Tabfilter from './list'
+import Tabfilter from './List'
 import Fave from './Fave.js'
 
- 
-export default class Home extends Component{
-    
-    render(){
-      return(
-        <div>
-        <Navbar/>
-        <MainCarousel/>
-       	<Tabfilter/>
-       	<Fave/>
-        </div>
-      );
-    }
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <MainCarousel />
+        <Tabfilter />
+        <Fave />
+      </div>
+    );
+  }
 }
